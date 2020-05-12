dce1c9b9e5e74a6e8351417f5dacec674220d569
BhopMC <41817945+BhopMC@users.noreply.github.com>
2020-05-12 13:40:07 +0300

    nigga

---
+++ shit temple/ac.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ac
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.keybinds = New System.Windows.Forms.Timer(Me.components)
+        Me.HexTheme1 = New dogshit.HexTheme()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.HexClose1 = New dogshit.HexClose()
+        Me.HexMini1 = New dogshit.HexMini()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.HexButton1 = New dogshit.HexButton()
+        Me.HexTheme1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'keybinds
+        '
+        '
+        'HexTheme1
+        '
+        Me.HexTheme1.Controls.Add(Me.Label3)
+        Me.HexTheme1.Controls.Add(Me.Label2)
+        Me.HexTheme1.Controls.Add(Me.HexClose1)
+        Me.HexTheme1.Controls.Add(Me.HexMini1)
+        Me.HexTheme1.Controls.Add(Me.Label1)
+        Me.HexTheme1.Controls.Add(Me.HexButton1)
+        Me.HexTheme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.HexTheme1.Location = New System.Drawing.Point(0, 0)
+        Me.HexTheme1.Name = "HexTheme1"
+        Me.HexTheme1.Size = New System.Drawing.Size(229, 220)
+        Me.HexTheme1.TabIndex = 0
+        Me.HexTheme1.Text = "dog shit clicker"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Arial Narrow", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(93, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 15)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Hide: F5"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(69, 113)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Enable / Disable: F4"
+        '
+        'HexClose1
+        '
+        Me.HexClose1.Location = New System.Drawing.Point(214, 10)
+        Me.HexClose1.Name = "HexClose1"
+        Me.HexClose1.Size = New System.Drawing.Size(12, 12)
+        Me.HexClose1.TabIndex = 5
+        Me.HexClose1.Text = "HexClose1"
+        '
+        'HexMini1
+        '
+        Me.HexMini1.Location = New System.Drawing.Point(196, 6)
+        Me.HexMini1.Name = "HexMini1"
+        Me.HexMini1.Size = New System.Drawing.Size(12, 12)
+        Me.HexMini1.TabIndex = 4
+        Me.HexMini1.Text = "HexMini1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(84, 198)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "@BhopMC"
+        '
+        'HexButton1
+        '
+        Me.HexButton1.Font = New System.Drawing.Font("Arial Narrow", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.HexButton1.ForeColor = System.Drawing.Color.White
+        Me.HexButton1.Location = New System.Drawing.Point(22, 80)
+        Me.HexButton1.Name = "HexButton1"
+        Me.HexButton1.Size = New System.Drawing.Size(186, 30)
+        Me.HexButton1.TabIndex = 0
+        Me.HexButton1.Text = "Start"
+        '
+        'ac
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(229, 220)
+        Me.Controls.Add(Me.HexTheme1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "ac"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "ac"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.HexTheme1.ResumeLayout(False)
+        Me.HexTheme1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents HexTheme1 As HexTheme
+    Friend WithEvents HexButton1 As HexButton
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents HexClose1 As HexClose
+    Friend WithEvents HexMini1 As HexMini
+    Friend WithEvents keybinds As Timer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ shit temple/ac.vb
@@ -0,0 +1,111 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+
+Public Class ac
+    Private Declare Function apimouse_event Lib "user32" Alias "mouse_event" (ByVal dwFlags As Int32, ByVal dX As Int32, ByVal dY As Int32, ByVal cButtons As Int32, ByVal dwExtraInfo As Int32) As Boolean
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal key As Integer) As Integer
+
+    Public Const MOUSEEVENTF_LEFTDOWN = &H2
+    Public Const MOUSEEVENTF_LEFTUP = &H4
+    Public Const MOUSEEVENTF_MIDDLEDOWN = &H20
+    Public Const MOUSEEVENTF_MIDDLEUP = &H40
+    Public Const MOUSEEVENTF_RIGHTDOWN = &H8
+    Public Const MOUSEEVENTF_RIGHTUP = &H10
+    Public Const MOUSEEVENTF_MOVE = &H1
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If MouseButtons = MouseButtons.Left Then
+            apimouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+            apimouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        End If
+    End Sub
+
+    Private Sub HexButton1_Click(sender As Object, e As EventArgs) Handles HexButton1.Click
+        If HexButton1.Text = "Stop" Then
+            HexButton1.Text = "Start"
+            Timer1.Stop()
+        ElseIf HexButton1.Text = "Start" Then
+            HexButton1.Text = "Stop"
+            Timer1.Start()
+        End If
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        Process.Start("https://youtube.com/bhopminecraft")
+    End Sub
+
+    Private Sub ac_Load(sender As Object, e As EventArgs) Handles Me.Load
+        keybinds.Start()
+
+        Timer1.Interval = 65
+    End Sub
+
+    Private Sub ac_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+
+        Dim nameofexe As String = AppDomain.CurrentDomain.FriendlyName  'Deletes from Prefetch The File.
+        Dim d As DirectoryInfo = New DirectoryInfo("C:\Windows\Prefetch")
+        Dim Files As FileInfo() = d.GetFiles(nameofexe & "*")
+
+        For Each file As FileInfo In Files
+            file.Delete()
+        Next
+
+        Me.Close()
+
+        HexButton1.Dispose()
+        HexTheme1.Dispose()
+        HexClose1.Dispose()
+
+
+    End Sub
+
+    Private Sub Keybinds_Tick(sender As Object, e As EventArgs) Handles keybinds.Tick 'Keybinds
+        If GetAsyncKeyState(Keys.F4) Then
+            If Timer1.Enabled = True Then
+                HexButton1.Text = "Start"
+                Timer1.Stop()
+            Else
+                HexButton1.Text = "Stop"
+                Timer1.Start()
+            End If
+        End If
+
+        If GetAsyncKeyState(Keys.F5) Then
+            If Me.Visible = True Then
+                Me.Visible = False
+                Me.ShowInTaskbar = False
+            Else
+                Me.Visible = True
+                Me.ShowInTaskbar = True
+            End If
+        End If
+    End Sub
+
+
+    Private Sub HexClose1_Click(sender As Object, e As EventArgs) Handles HexClose1.Click
+
+        Me.Close()
+
+        Dim nameofexe As String = AppDomain.CurrentDomain.FriendlyName  'Deletes from Prefetch The File.
+        Dim d As DirectoryInfo = New DirectoryInfo("C:\Windows\Prefetch")
+        Dim Files As FileInfo() = d.GetFiles(nameofexe & "*")
+
+        For Each file As FileInfo In Files
+            file.Delete()
+        Next
+
+
+        'Form
+        HexMini1.Dispose()
+        HexButton1.Dispose()
+        HexTheme1.Dispose()
+        HexClose1.Dispose()
+
+        'Label
+        Label1.Dispose()
+        Label2.Dispose()
+        Label3.Dispose()
+
+
+    End Sub
+End Class
